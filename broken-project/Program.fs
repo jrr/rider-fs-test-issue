b5250bde0eda57e774f98497f6d1f56e77586134
John Ruble <johnruble@gmail.com>
2018-06-15 13:47:30 -0400

    further reduce

--- broken-project/Program.fs
+++ broken-project/Program.fs
@@ -1,15 +1,7 @@
-module BrokenProject
-
-let dividesBy (n: int, m: int): bool = n % m = 0
-
-let leapYear (year: int): bool =
-    dividesBy(year, 4) && (not(dividesBy(year, 100)) || dividesBy(year, 400))
-    
 open FsUnit.Xunit
 open Xunit
 
 [<Fact>]
-let ``Year not divisible by 4: common year`` () =
-    leapYear 2015 |> should equal false
+let ``name that has a colon : in it`` () =
+    true |> should equal true
 
-let [<EntryPoint>] main _ = 0
--- working-project/Program.fs
+++ working-project/Program.fs
@@ -1,26 +1,7 @@
-module WorkingProject
-
-let valid triangle =
-    let sorted = triangle |> List.sort |> List.rev
-    sorted.[0] < sorted.[1] + sorted.[2] && sorted.[2] <> 0.0
-    
-let distinctLengths = List.distinct >> List.length
-
-let validTriangleWithDistinctLengths lenFn triangle =
-    valid triangle && (triangle |> distinctLengths |> lenFn )
-    
-let equilateral = validTriangleWithDistinctLengths (fun n -> n = 1)
-
-let isosceles = validTriangleWithDistinctLengths (fun n -> n < 3)
-
-let scalene = validTriangleWithDistinctLengths (fun n -> n = 3)
-
-
 open FsUnit.Xunit
 open Xunit
 
 [<Fact>]
-let ``Equilateral returns true if all sides are equal`` () =
-    equilateral [2.0; 2.0; 2.0] |> should equal true
+let ``name that has spaces but no punctuation`` () =
+    true |> should equal true
 
-let [<EntryPoint>] main _ = 0
